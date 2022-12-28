3ca08f0864501958807908708faf70ae2a8a69e6
Youssef3B <3b.youssef@gmail.com>
2022-12-28 16:37:30 +0100

    bug

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ const checkSize = () => {
   if ($(window).width() < 992) {
     $("nav").removeClass("position-fixed")
     $("nav").css("min-width", "250px")
+    $('.link_nav a').on("click", () => {
+      $(".navbar-collapse").removeClass("show");  
+      $(".navbar-toggler").addClass("collapsed");
+    })    
   } else if ($(window).width() < 1590) {
     $("nav").addClass("position-fixed")
     $("nav").css("min-width", "250px")
